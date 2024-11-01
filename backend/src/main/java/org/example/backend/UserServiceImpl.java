cafed20296e4ac73c8a373cdd937bae5c04f2d0e
murhaf2211 <orfali.murhaf@web.de>
2024-11-01 18:04:51 +0100

    Backend update UserServiceImpl.java add log.info for all

--- backend/src/main/java/org/example/backend/UserServiceImpl.java
+++ backend/src/main/java/org/example/backend/UserServiceImpl.java
@@ -18,28 +18,33 @@ public  class UserServiceImpl implements UserService {
 
    @Override
    public User saveUser(User user) {
+      log.info("save user {}", user.getName());
       return userRepo.save(user);
    }
 
    @Override
    public Role saveRole(Role role) {
+      log.info("save role {}", role.getName());
       return rolRepo.save(role);
    }
 
    @Override
    public void addRoleToUser(String username, String roleName) {
-   User user = userRepo.findByUsername(username);
-   Role role = rolRepo.findByName(roleName);
-   user.getRoles().add(role);
+      log.info("add role {} to user {}", roleName, username);
+      User user = userRepo.findByUsername(username);
+      Role role = rolRepo.findByName(roleName);
+      user.getRoles().add(role);
    }
 
    @Override
    public User getUser(String username) {
+      log.info("get user {}", username);
       return userRepo.findByUsername(username);
    }
 
    @Override
    public List<User> getUsers() {
+      log.info("get all users");
       return userRepo.findAll();
    }
 }
