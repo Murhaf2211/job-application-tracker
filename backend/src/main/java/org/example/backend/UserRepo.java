cb19949c456214aa5b5fb05725a648616ac1d499
murhaf2211 <orfali.murhaf@web.de>
2024-11-01 17:37:13 +0100

    Backend add  interfaces RoleRepo.java and UserRepo.java

---
+++ backend/src/main/java/org/example/backend/UserRepo.java
@@ -0,0 +1,7 @@
+package org.example.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
