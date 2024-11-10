20741dabfa1bb8b770617e99c07c93ef2598f08b
murhaf2211 <orfali.murhaf@web.de>
2024-11-10 20:07:11 +0100

    delete security files to update backend

--- backend/src/main/java/org/example/backend/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepo extends MongoRepository<ApiRole, String> {
-    ApiRole findByName(String name); // Custom method to find a role by name
-}
